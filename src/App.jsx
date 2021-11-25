9f556c7ee2ba424ab489bfe7ee94326652895b00
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-11-25 17:34:28 +0900

    inline style

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+import { InlineStyle } from "./components/InlineStyle";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <InlineStyle />
+    </div>
+  );
+};
