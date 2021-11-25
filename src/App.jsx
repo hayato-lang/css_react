e0a2d630b4aedd227ab2c41e9c49a649f9e45b80
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-11-25 19:11:17 +0900

    CssModules

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './App.css';
 import { InlineStyle } from "./components/InlineStyle";
-
+import { CssModules } from "./components/CssModules";
 export const App = () => {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules/>
     </div>
   );
 };
