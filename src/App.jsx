0a69eec8f7e084336f5ce7149576961c3c92e4a7
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-11-25 19:36:38 +0900

    Styled Jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import './App.css';
 import { InlineStyle } from "./components/InlineStyle";
 import { CssModules } from "./components/CssModules";
+import {StyledJsx} from "./components/StyledJsx";
 export const App = () => {
   return (
     <div className="App">
       <InlineStyle />
       <CssModules/>
+      <StyledJsx/>
     </div>
   );
 };
