72521a8d663303c0662d123c321a34a01792c06f
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-11-26 09:03:14 +0900

    styled components

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import { InlineStyle } from "./components/InlineStyle";
 import { CssModules } from "./components/CssModules";
 import {StyledJsx} from "./components/StyledJsx";
+import {StyledComponents} from "./components/StyledComponents";
 export const App = () => {
   return (
     <div className="App">
       <InlineStyle />
       <CssModules/>
       <StyledJsx/>
+      <StyledComponents/>
     </div>
   );
 };
